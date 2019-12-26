8f72af2d63f2dac4b05101b4dc5bb6205c888509
Kalkidan Mesfin <mesfinkalkidan@gmail.com>
2019-12-26 20:45:05 +0300

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatInputModule,MatSnackBarModule, MatChipsModule, MatDialogModule, MatPaginatorModule, MatFormFieldModule, MatSortModule, MatDatepickerModule, MatNativeDateModule, MatRadioModule, MatStepperModule, MatCheckboxModule, MatListModule, MatBadgeModule, MatCardModule, MatButtonModule, MatGridListModule, MatSelectModule, MatMenuModule, MatSidenavModule, MatIconModule, MatToolbarModule, MatTableModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/Dashboard/home/home.component';
+import { LoginComponent } from './Component/Auth/login/login.component';
+import { RegisterComponent } from './Component/Auth/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatChipsModule,
+    MatDialogModule, 
+    MatPaginatorModule, 
+    MatFormFieldModule, 
+    MatSortModule, 
+    MatDatepickerModule, 
+    MatNativeDateModule, 
+    MatRadioModule, 
+    MatStepperModule, 
+    MatCheckboxModule, 
+    MatListModule, 
+    MatBadgeModule, 
+    MatCardModule, 
+    MatButtonModule, 
+    MatGridListModule, 
+    MatSelectModule, 
+    MatMenuModule, 
+    MatSidenavModule, 
+    MatIconModule, 
+    MatToolbarModule, 
+    MatTableModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
